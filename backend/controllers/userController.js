df0c1415eb1daee7df050542b8c0c396259ec3ad
praveen <mspraveenkumar77@gmail.com>
2023-06-10 21:07:46 +0530

    mongodb-configured

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 /**
  * @desc   Auth user/set token
@@ -9,4 +10,46 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+/**
+ * @desc   Register a new User
+ * @route  POST /api/users/
+ * @access Public
+ */
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+/**
+ * @desc   Logout User
+ * @route  POST /api/users/logout
+ * @access Public
+ */
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+/**
+ * @desc   Get User Profile
+ * @route  GET /api/users/profile
+ * @access Private
+ */
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get User Profile" });
+});
+
+/**
+ * @desc   Update User Profile
+ * @route  PUT /api/users/profile
+ * @access Private
+ */
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
 
+router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+// router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.get("/profile", getUserProfile);
+router.put("/profile", updateUserProfile);
 export default router;
